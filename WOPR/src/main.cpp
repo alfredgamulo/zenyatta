54324f6bc84b0623fe91d811c79ff5abc47d2db2
Alfred Gamulo <alfred.gamulo@gmail.com>
2026-01-10 22:24:29 -0500

    feat: add modified WOPR example

---
+++ WOPR/src/main.cpp
@@ -0,0 +1,147 @@
+// Program to exercise the MD_MAX72XX library
+//
+// Uses most of the functions in the library
+#include <Arduino.h>
+#include <MD_MAX72xx.h>
+#include <WiFi.h>
+#include "time.h"
+#include "credentials.h"
+
+// Turn on debug statements to the serial output
+#define DEBUG 1
+
+#if DEBUG
+#define PRINT(s, x)     \
+  {                     \
+    Serial.print(F(s)); \
+    Serial.print(x);    \
+  }
+#define PRINTS(x) Serial.print(F(x))
+#define PRINTD(x) Serial.println(x, DEC)
+
+#else
+#define PRINT(s, x)
+#define PRINTS(x)
+#define PRINTD(x)
+
+#endif
+
+// Define the number of devices we have in the chain and the hardware interface
+// NOTE: These pin numbers will probably not work with your hardware and may
+// need to be adapted
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 12
+
+#define CLK_PIN 4  // or SCK
+#define DATA_PIN 6 // or MOSI
+#define CS_PIN 7   // or SS
+
+// SPI hardware interface
+MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+// Specific SPI hardware interface
+// MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, SPI1, CS_PIN, MAX_DEVICES);
+// Arbitrary pins
+// MD_MAX72XX mx = MD_MAX72XX(HARDWARE_TYPE, DATA_PIN, CLK_PIN, CS_PIN, MAX_DEVICES);
+
+// We always wait a bit between updates of the display
+#define DELAYTIME 100 // in milliseconds
+#define ROW_SIZE 8    // MAX72XX modules are 8x8 LED matrices
+
+// Declare scrollText and bounce functions
+void scrollText(const char *p);
+const char *getTimeString();
+
+void scrollText(const char *p)
+{
+  uint8_t charWidth;
+  uint8_t cBuf[8]; // this should be ok for all built-in fonts
+
+  PRINTS("\nScrolling text");
+  mx.clear();
+
+  while (*p != '\0')
+  {
+    charWidth = mx.getChar(*p++, sizeof(cBuf) / sizeof(cBuf[0]), cBuf);
+
+    for (uint8_t i = 0; i <= charWidth; i++) // allow space between characters
+    {
+      mx.transform(MD_MAX72XX::TSL);
+      mx.setColumn(0, (i < charWidth) ? cBuf[i] : 0);
+      delay(DELAYTIME);
+    }
+  }
+
+  // Keep shifting until the display is clear
+  // We shift for (Number of Devices * 8 columns per device)
+  for (uint16_t i = 0; i < (MAX_DEVICES * 8); i++)
+  {
+    mx.transform(MD_MAX72XX::TSL);
+    mx.setColumn(0, 0); // Inject empty (blank) columns
+    delay(DELAYTIME);
+  }
+}
+
+const char *ssid = WIFI_SSID;
+const char *password = WIFI_PASSWORD;
+
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = -18000;   // Eastern Standard Time is -5 hours. (5 * 3600)
+const int daylightOffset_sec = 3600; // 1 hour for Daylight Savings
+
+const char *getTimeString()
+{
+  static char timeString[10];
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    return "Err:Time";
+  }
+  // This formats the time as Hour:Minute (e.g., 14:30)
+  strftime(timeString, 10, "%H:%M", &timeinfo);
+  return timeString;
+}
+
+void setup()
+{
+#if DEBUG
+  Serial.begin(57600);
+#endif
+  PRINTS("\n[MD_MAX72XX Test & Demo]");
+
+  if (!mx.begin())
+    PRINTS("\nMD_MAX72XX initialization failed");
+
+  PRINTS("\nConnecting to Wifi...");
+  WiFi.begin(ssid, password);
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED && attempts < 40) // 20 second timeout
+  {
+    delay(500);
+    PRINTS(".");
+    attempts++;
+  }
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    PRINTS("\nWiFi Connected");
+    configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  }
+  else
+  {
+    PRINTS("\nWiFi Connection Failed!");
+  }
+}
+
+void loop()
+{
+#if 1
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    scrollText(getTimeString());
+  }
+  else
+  {
+    scrollText("WiFi Error");
+  }
+  delay(1000); // Wait 1 second before next update
+#endif
+}
